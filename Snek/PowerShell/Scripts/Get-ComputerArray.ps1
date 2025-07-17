2666293b89c73773df2e500712ea0cc5bceb81d2
Bodenner <204929239+tbodenner@users.noreply.github.com>
2025-07-17 10:57:57 -0700

    Updated AD computer array script to include multiple domains.

--- Snek/PowerShell/Scripts/Get-ComputerArray.ps1
+++ Snek/PowerShell/Scripts/Get-ComputerArray.ps1
@@ -1,4 +1,8 @@
-$SearchBase = "OU=VISN18,DC=v18,DC=med,DC=va,DC=gov"
+$Domains = @('va.gov', 'v18.med.va.gov')
 $Filter = "Name -like 'PRE-*'"
-$ComputerNames = (Get-ADComputer -Filter $Filter -SearchBase $SearchBase | Where-Object { $_.Enabled -eq $true }).Name
+$ComputerNames = @()
+foreach ($Domain in $Domains) {
+    $Server = Get-ADDomainController -Discover -DomainName $Domain
+    $ComputerNames += (Get-ADComputer -Filter $Filter -Server $Server | Where-Object { $_.Enabled -eq $true }).Name
+}
 $ComputerNames
