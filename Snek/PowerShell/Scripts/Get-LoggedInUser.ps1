eb0626c02b87913b569f5566e61a3da073b916df
tbodenner <204929239+tbodenner@users.noreply.github.com>
2025-12-31 09:17:56 -0700

    Updated the script to get the logged in user to use quser.exe instead of a remote PowerShell session.

--- Snek/PowerShell/Scripts/Get-LoggedInUser.ps1
+++ Snek/PowerShell/Scripts/Get-LoggedInUser.ps1
@@ -1,3 +1,4 @@
+<#
 try {
     # change our default settings for our remote session used by invoke-command
     $PssOptions = New-PSSessionOption -MaxConnectionRetryCount 0 -OpenTimeout 10000 -OperationTimeout 30000
@@ -28,3 +29,33 @@ if (($User | Out-String) -like "*WARNING*") {
 
 # return the result
 $User
+#>
+
+# try to get our logged in user
+try {
+    # use quser to get the logged in user
+    $QResult = quser.exe /SERVER:"PYTHON_COMPUTERNAME"
+
+    # check if we received a result
+    if (($null -eq $QResult) -or ($QResult -eq "")) {
+        # return an empty string if we had no result
+        ''
+    }
+    else {
+        # otherwise, check if our result has two or more lines
+        if ($QResult.Length -ge 2) {
+            # get our user from our second line
+            $User = $QResult[1].Substring(1,12)
+            # return our user
+            $User
+        }
+        else {
+            # if we didn't receive enough lines, return an empty string
+            ''
+        }
+    }
+}
+catch {
+    # on any errors, return an error
+    'Error'
+}
